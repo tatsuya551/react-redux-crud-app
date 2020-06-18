faaf0c9ded80bc5c860fb5e5e9e53bd496c5ab61
Seki Tatsuya <seki0515ya@gmail.com>
2020-06-18 14:38:11 +0900

    implement delete feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,24 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+  READ_EVENTS,
+  CREATE_EVENT,
+  READ_EVENT,
+  DELETE_EVENT,
+  UPDATE_EVENT
+} from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data }
+    case DELETE_EVENT:
+      delete events[action.id]
+      return { ...events }
     default:
       return events
   }
